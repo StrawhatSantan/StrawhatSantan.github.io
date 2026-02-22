5d6530cc48d005e820bf83f85d75dc1cf3901b17
StrawhatSantan <97388567+StrawhatSantan@users.noreply.github.com>
2026-02-22 12:02:07 +0000

    Deploying to gh-pages from @ StrawhatSantan/StrawhatSantan.github.io@3364cabeab4a19ad6ff26844ad2c3f4d4c2d70dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -485,13 +485,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/bolutife-atoki-a2b7a3196", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
